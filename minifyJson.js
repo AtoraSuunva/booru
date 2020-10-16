6adb5bcf5162b42fc4f2f4e8d4b4887eb300dfec
AtlasTheBot <AtlasTheBot@users.noreply.github.com>
2020-10-15 21:22:42 -0400

    Migrate from terser-folder, update circleci, update dependencies

--- minifyJson.js
+++
@@ -1,8 +0,0 @@
-(async () => {
-  const fs = require('fs').promises
-  const [,,from,to] = process.argv
-  if (!from || !to) throw new Error('No file to minify, or no output')
-  const content = await fs.readFile(from, 'utf8')
-  await fs.writeFile(to, JSON.stringify(JSON.parse(content)), 'utf8')
-  console.log(`Minified ${from} to ${to}!`)
-})()
