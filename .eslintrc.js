5e57b91de4606ef49ed87dcdad05fc5cd5ee02be
AtoraSuunva <at@atora.dev>
2022-01-12 23:53:31 -0500

    chore(config): update project typescript/eslint/prettier files for consistency

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,42 +1,33 @@
 module.exports = {
-  env: {
-    es6: true,
-    node: true,
-  },
+  root: true,
   parser: '@typescript-eslint/parser',
   parserOptions: {
     tsconfigRootDir: __dirname,
-    project: 'tsconfig.eslint.json',
-    sourceType: 'module',
+    project: ['./tsconfig.json'],
   },
+  plugins: ['@typescript-eslint'],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
     'prettier',
   ],
-  plugins: [
-    '@typescript-eslint',
-    'eslint-plugin-import',
-    'eslint-plugin-no-null',
-    'eslint-plugin-prefer-arrow',
-    'eslint-plugin-jsdoc',
-  ],
   rules: {
-    // Disabling rules like this is a terrible terrible idea but I want to
-    // rewrite booru and don't feel like fixing all these eslint warnings right
-    // now
-    // This was written when I was initially working with Typescript, I've
-    // gotten much better at properly using it now (and also working with
-    // eslint from the beginning)
-    // So next version expect better type safety 😎
-    '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/restrict-template-expressions': 'off',
+    // Re-enable once fixed
+    'import/no-unresolved': 'off',
+
+    // Re-enable for the rewrite where I remove all `any`
+    '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-unsafe-assignment': 'off',
     '@typescript-eslint/no-unsafe-member-access': 'off',
-    '@typescript-eslint/no-unsafe-return': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-unsafe-call': 'off',
-    '@typescript-eslint/restrict-template-expressions': 'off',
+    '@typescript-eslint/no-unsafe-return': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/ban-types': 'off',
+    'import/no-named-as-default': 'off',
   },
 }
