763cd6c4a54e777234420c3c694b1a0dc0112264
AtoraSuunva <at@atora.dev>
2022-09-11 15:12:11 -0400

    fix: export some important types + mark hypnohub as not supporting random

--- minify.js
+++ minify.js
@@ -10,10 +10,10 @@ const { stat, readdir, readFile, writeFile } = require('fs').promises
 async function getAllFiles({ path }) {
   const files = await readdir(path)
 
-  const fPromises = files.map(async file => {
+  const fPromises = files.map(async (file) => {
     if ((await stat(`${path}/${file}`)).isDirectory()) {
       return getAllFiles({ path: `${path}/${file}` })
-    } else if (file.match(/\.js(on)?$/)) {
+    } else if (file.match(/\.json$/)) {
       return join(resolve(), path, file)
     }
     return []
@@ -22,26 +22,25 @@ async function getAllFiles({ path }) {
   return (await Promise.all(fPromises)).flat()
 }
 
-
 async function minifyFiles(filePaths) {
-  const fPromises = filePaths.map(async path => {
-    switch(extname(path)) {
-      case '.js':
-        const miniJS = await minify(await readFile(path, 'utf8'))
-        return writeFile(path, miniJS.code, 'utf8')
+  const fPromises = filePaths.map(async (path) => {
+    switch (extname(path)) {
+      // case '.js':
+      //   const miniJS = await minify(await readFile(path, 'utf8'))
+      //   return writeFile(path, miniJS.code, 'utf8')
 
       case '.json':
         const content = (await readFile(path, 'utf8')).trim()
         const miniJSON = JSON.stringify(JSON.parse(content))
         return writeFile(path, miniJSON, 'utf8')
-      }
-    })
+    }
+  })
 
   return Promise.all(fPromises)
 }
 
-(async () => {
-  const files = await getAllFiles({ path: "./dist" })
+;(async () => {
+  const files = await getAllFiles({ path: './dist' })
   console.log('Minifying files: ')
   console.log(files.join('\n'))
   await minifyFiles(files)
